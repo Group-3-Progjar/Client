3e6e114aee2d20e3d7bf1391510943180373935b
mikhaelarya <mikabel.arya@gmail.com>
2024-06-21 07:51:38 +0700

    add obstacle logic

--- game/objects/obstacles/obstacle.py
+++ game/objects/obstacles/obstacle.py
@@ -1,35 +1,51 @@
 from game.objects.gameobject import GameObject
+import random
+import itertools
 
 class Obstacle(GameObject):
     def __init__(self, scene, image, x, y):
         super().__init__(scene, image, x, y)
 
+        self.initX = x
+        self.initY = y
+
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
+    #need to fix rect and x and y
+    def update(self):
+        if self.scene.game.isPlay():
+            self.x -= 10
+        self.rect.x = self.x
+            
 
-    def checkCollision(entity):
+    def checkCollision(self, entity):
         return entity.rect.colliderect(self.rect)
 
 
 class Obstacles:
     def __init__(self, obstacles):
-        self.obstacles = []
+        self.obstacles = obstacles
 
 
     def update(self):
         for obstacle in self.obstacles:
-            obstacle.update()
+            if obstacle.scene.game.isPlay():
+                obstacle.update()
+                if obstacle.x < -250:
+                    newX = obstacle.initX + random.randrange(-200, 1000, 100)
+                    if newX < 800:
+                        newX += 800
+                    obstacle.x = newX
 
     def handleEvents(self, events):
         pass
 
-    def render():
+    def render(self):
         for obstacle in self.obstacles:
             obstacle.render()
 
-    def checkCollision(entity, trueFunc, tArgs, falseFunc, fArgs):
+
+    def checkCollision(self, entity, trueFunc, tArgs):
         for obstacle in self.obstacles:
             if obstacle.checkCollision(entity):
                 trueFunc(*tArgs)
-            else:
-                falseFunc(*fArgs)
